3046b9c203652ca0706cecaddb997e6fe60c9258
Adina Wagner <adina.wagner@t-online.de>
2020-09-27 07:56:51 +0200

    MNT: Switch to more recent neurodebian
    The previous neurodebian installed datalad with git-annex 7.x which wasn't able to work with version 8 repositories.

--- envs/generate_simg.sh
+++ envs/generate_simg.sh
@@ -5,7 +5,7 @@ set -e
 # Generate a singularity image with Neurodocker
 
 docker run --rm kaczmarj/neurodocker:master generate singularity \
---base neurodebian:buster-non-free \
+--base neurodebian:bullseye-non-free \
 --pkg-manager apt \
 --install datalad python3-tk python3-pandas python3-sklearn python3-seaborn \
-> envs/Singularity.1
+> envs/Singularity.2
